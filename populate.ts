160335f0c7091dd8012c3e7e10fe2e7e0259a4fa
Gabriel Cardona <gabriel@avalabs.org>
2020-08-27 22:01:19 -0700

    Cleanin up the code.

--- populate.ts
+++ populate.ts
@@ -57,7 +57,7 @@ const createNFT = async (name: string, symbol: string, addresses: Buffer[], crea
     memo
   )
 
-  const tx: Tx = unsignedTx.sign(myKeychain);
+  const tx: Tx = unsignedTx.sign(myKeychain)
   const txid: string = await avm.issueTx(tx)
   console.log(`Create NFT Asset Success: ${txid}`)
   return txid;
@@ -65,14 +65,13 @@ const createNFT = async (name: string, symbol: string, addresses: Buffer[], crea
 
 const mintNFT = async (txid: string, addresses: Buffer[], addressStrings: string[], payload:any): Promise<string> => {
   console.log(`Creating NFT Mint Operation #1...`)
-  let utxoSet = await avm.getUTXOs(addressStrings);
-  let utxoids = utxoSet.getUTXOIDs();
-  // let result: string = utxoids[0];
-  let result: string = "";
+  const utxoSet = await avm.getUTXOs(addressStrings)
+  const utxoids = utxoSet.getUTXOIDs()
+  let result: string = ""
 
   // scan utxos and find the nft one
   for (let index: number = 0; index < utxoids.length; ++index) {
-    let value = utxoids[index];
+    let value = utxoids[index]
     if (value.substring(0, 10) === txid.substring(0, 10)) {
       result = value
       break
@@ -82,77 +81,57 @@ const mintNFT = async (txid: string, addresses: Buffer[], addressStrings: string
   console.log(result)
 
   const groupID: number = 0
-  let locktime = new BN(0);
-  let threshold = 1;
+  const locktime: BN = new BN(0)
+  const threshold: number = 1
 
-  let outputOwners:Array<OutputOwners> = []
+  const outputOwners:Array<OutputOwners> = []
   outputOwners.push(new OutputOwners(addresses, locktime, threshold))
 
-  let unsignedTx = await avm.buildCreateNFTMintTx(
+  const unsignedTx = await avm.buildCreateNFTMintTx(
     utxoSet,
     addressStrings,
     addressStrings,
     result,
-    // fee,
     groupID,
     payload.toBuffer(),
-    undefined,
-    // addressStrings,
-    UnixNow(),
-    locktime,
-    threshold,
+    memo
   )
 
-  let tx = unsignedTx.sign(avm.keyChain());
-  // let tx =  avm.keyChain().signTx(unsignedTx)
-  // console.log(tx.toBuffer().toString('hex'))
-  let mintTxid:string = await avm.issueTx(tx)
+  const tx: Tx = unsignedTx.sign(avm.keyChain())
+  const mintTxid: string = await avm.issueTx(tx)
   console.log(`NFT Mint Operation Success #1: ${mintTxid}`)
-  return mintTxid;
+  return mintTxid
 }
 
 const transferNFT = async (utxoId: string, toAddresses: string[]): Promise<string> => {
-    // let fee = new BN(0);
-    // let locktime = new BN(0);
-    // let threshold = 1;
+  const fromAddrs: Buffer[] = myKeychain.getAddresses()
+  const fromAddrsStr: string[] = myKeychain.getAddressStrings()
+  const utxos: UTXOSet = await avm.getUTXOs(fromAddrsStr)
+  const utxoids: string[] = utxos.getUTXOIDs()
 
-    let fromAddrs = myKeychain.getAddresses();
-    let fromAddrsStr = myKeychain.getAddressStrings();
-    let utxos = await avm.getUTXOs(fromAddrsStr);
-    let utxoids = utxos.getUTXOIDs();
+  console.log(`Utxo ids: `,utxoids)
+  // let sourceTxId: string = ""
+  let sourceTxId: string = utxoids[0]
 
-    console.log(`Utxo ids: `,utxoids);
-    let sourceTxId: string = "";
-    // let sourceTxId: string = utxoids[1];
-
-    for (let index: number = 0; index < utxoids.length; ++index) {
-        let value = utxoids[index];
-        if (value.substring(0, 10) === utxoId.substring(0, 10)) {
-            sourceTxId = value
-            break;
-        }
+  for (let index: number = 0; index < utxoids.length; ++index) {
+    let value = utxoids[index]
+    if (value.substring(0, 10) === utxoId.substring(0, 10)) {
+      sourceTxId = value
+      break
     }
+  }
 
-    console.log("Source tx: ",sourceTxId);
-
-    let unsignedTx = await avm.buildNFTTransferTx(
-        utxos,
-        toAddresses,
-        fromAddrsStr,
-        sourceTxId,
-        // fee,
-        // fromAddrsStr,
-        // UnixNow(),
-        // locktime,
-        // threshold
-    )
-
-    let tx = unsignedTx.sign(avm.keyChain());
-
-    // let tx =  avm.keyChain().signTx(unsignedTx)
-    let txid = await avm.issueTx(tx)
-    console.log(`NFT Transfer Operation Success: ${txid}`)
-    return txid;
+  console.log("Source tx: ",sourceTxId);
+  const unsignedTx: UnsignedTx = await avm.buildNFTTransferTx(
+    utxos,
+    toAddresses,
+    fromAddrsStr,
+    sourceTxId
+  )
+  const tx: Tx = unsignedTx.sign(avm.keyChain())
+  const txid: string = await avm.issueTx(tx)
+  console.log(`NFT Transfer Operation Success: ${txid}`)
+  return txid
 }
 
 // the goods
@@ -164,21 +143,16 @@ const main = async (): Promise<any> => {
   let txId = await createNFT(name, symbol, addresses, addressStrings)
   await sleep(5000)
   // Minting the NFT
-  const payload: URLPayload = new URLPayload('https://i.imgur.com/m44RDjm.jpg')
+  const payload: URLPayload = new URLPayload('https://media.giphy.com/media/ZTjQgJGDiuJZS/source.gif')
   const mintTxId: string = await mintNFT(txId, addresses, addressStrings, payload)
-
-//   setTimeout(async () =>{
-    // const payload: UTF8Payload = new UTF8Payload(`Test test`)
-    // const payload: URLPayload = new URLPayload('https://upload.wikimedia.org/wikipedia/commons/f/f7/Bananas.svg')
-    // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/QmSRti2HK95NXWYG3t3he7UK7hkgw8w9TdqPc6hi5euV1p/prism/33a.gif')
-    // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/QmSRti2HK95NXWYG3t3he7UK7hkgw8w9TdqPc6hi5euV1p/sketch/30a.gif')
-    // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/Qmbxify5MLarmoxTZTCDzSsUY6tRFwt1zbRBeW3gowUNJX/tigerscratch/11b.gif')
-
-    // setTimeout(async () => {
-    //   let nftTo = ["X-local1tfemk93pgdlc5r3xmawt20nvksjgg7k8q7cd4y"]
-    //     await transferNFT( mintTxId, nftTo)
-    // }, 5000)
-//   }, 5000)
+  await sleep(5000)
+  const nftTo: string[] = ["X-local1tfemk93pgdlc5r3xmawt20nvksjgg7k8q7cd4y"]
+  await transferNFT(mintTxId, nftTo)
+  // const payload: UTF8Payload = new UTF8Payload(`Test test`)
+  // const payload: URLPayload = new URLPayload('https://upload.wikimedia.org/wikipedia/commons/f/f7/Bananas.svg')
+  // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/QmSRti2HK95NXWYG3t3he7UK7hkgw8w9TdqPc6hi5euV1p/prism/33a.gif')
+  // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/QmSRti2HK95NXWYG3t3he7UK7hkgw8w9TdqPc6hi5euV1p/sketch/30a.gif')
+  // const payload: URLPayload = new URLPayload('https://cloudflare-ipfs.com/ipfs/Qmbxify5MLarmoxTZTCDzSsUY6tRFwt1zbRBeW3gowUNJX/tigerscratch/11b.gif')
 }
 
 main()
