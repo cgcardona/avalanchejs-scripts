c516f11e091c68e41ef4f6242b1b6a0394d64d8c
Gabriel Cardona <gabriel@avalabs.org>
2021-01-07 07:49:12 -0800

    Add code to test codecid 00 00 and 00 01.

--- transactions/codec01/avm-buildCreateAssetTx-ant.ts
+++ transactions/codec01/avm-buildCreateAssetTx-ant.ts
@@ -80,8 +80,19 @@ const main = async (): Promise<any> => {
     outputs,
     memo
   )
-  const tx: Tx = unsignedTx.sign(xKeychain)
-  const id: string = await xchain.issueTx(tx)
+
+  // start uncomment for codecID 00 00
+  // const tx: Tx = unsignedTx.sign(xKeychain)
+  // const id: string = await xchain.issueTx(tx)
+  // stop uncomment for codecID 00 00
+
+  // start uncomment for codecID 00 01
+  const codecID: number = 1
+  const tx: Tx = unsignedTx.sign(xKeychain, codecID)
+  const cb58EncodedTx: string = bintools.cb58Encode(tx.toBuffer(codecID))
+  const id: string = await xchain.issueTx(cb58EncodedTx)
+  // stop uncomment for codecID 00 01
+
   console.log(id)
 }
   
--- transactions/codec01/avm-operationTx-mint-nft.ts
+++ transactions/codec01/avm-operationTx-mint-nft.ts
@@ -119,8 +119,19 @@ const main = async (): Promise<any> => {
     operations
   )
   const unsignedTx: UnsignedTx = new UnsignedTx(operationTx)
-  const tx: Tx = unsignedTx.sign(xKeychain)
-  const id: string = await xchain.issueTx(tx)
+
+  // start uncomment for codecID 00 00
+  // const tx: Tx = unsignedTx.sign(xKeychain)
+  // const id: string = await xchain.issueTx(tx)
+  // stop uncomment for codecID 00 00
+
+  // start uncomment for codecID 00 01
+  const codecID: number = 1
+  const tx: Tx = unsignedTx.sign(xKeychain, codecID)
+  const cb58EncodedTx: string = bintools.cb58Encode(tx.toBuffer(codecID))
+  const id: string = await xchain.issueTx(cb58EncodedTx)
+  // stop uncomment for codecID 00 01
+
   console.log(id)
 }
   
